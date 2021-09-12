2641187ceec6466daa9ab0a0ac98350a3072cd11
Hadi.sh <88891220+HadiDotSh@users.noreply.github.com>
2021-09-12 19:17:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 # By @HadiDotSh
-ALIAS="alias todo=\bash ~/.todo/bash-todolist.sh"
+ALIAS="alias todo=\"bash ~/.todo/bash-todolist.sh\""
 printf "\e[0;92m- \e[0m\e[1;77mBash TodoList\e[0;96m [By @HadiDotSh]\n\e[0m"
 mkdir "${HOME}/.todo"
 mv "todolist.sh" "${HOME}/.todo/bash-todolist.sh"
 printf "\n\e[0;92m? \e[0m\e[1;77mInstallation Done.\n\e[0m"
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-   ${ALIAS} >> ~/.zshrc
+   ${ALIAS} >> ${HOME}/.zshrc
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-   ${ALIAS} >> ~/.bashrc
+   ${ALIAS} >> ${HOME}/.bashrc
 else
    prinf "\e[0;92mDon't forget to add an alias to your bashrc or zshrc :\n\e[0;96malias todo=\"bash ~/.todo/bash-todolist.sh\"\n\n\e[0m"
 fi
